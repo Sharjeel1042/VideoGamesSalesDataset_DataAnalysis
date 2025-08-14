53047375dd4b326c4dc0c8d87d061cbb8472ab27
Syed Muhammad Sharjeel Farsheed <FA23-BCS-094@ISBSTUDENT.COMSATS.EDU.PK>
2025-08-14 13:00:03 +0400

    Initial Commit

--- barGraph_AvgGlobalSales VS Genre.py
+++ barGraph_AvgGlobalSales VS Genre.py
@@ -1,14 +1,24 @@
 from main import dfNonTime
 import matplotlib.pyplot as plt
 
-sales=dfNonTime.groupby('genre')['global_sales'].mean()
+
+plt.rcParams['xtick.labelsize']=10
+plt.rcParams['xtick.labelcolor']='red'
+plt.rcParams['ytick.labelsize']=10
+plt.rcParams['ytick.labelcolor']='red'
+plt.rcParams['font.family']='DejaVu Sans Mono'
+
+sales=dfNonTime.groupby('genre')['global_sales'].mean().sort_values(ascending=False)
 genre=sales.index.tolist()
 avgGlobalSales=sales.values.tolist()
 
 fig, ax=plt.subplots(figsize=(13,7),dpi=100)
 bars=ax.bar(genre,avgGlobalSales,width=0.5,color='yellow')
-plt.xlabel('Genre')
-plt.ylabel('Avg.Global Sales\n(millions)')
+plt.xlabel('Genre',fontdict={'fontsize':15,'font':'Comic Sans MS'})
+plt.ylabel('Avg.Global Sales\n(millions)',fontdict={'fontsize':15,'font':'Comic Sans MS'})
+
+
+
 for bar in bars:
     height=bar.get_height()
     ax.text(
@@ -18,8 +28,5 @@ for bar in bars:
     ha='center',
     va='bottom'
     )
-
-
-
 
 plt.show()
